59eaef55ec6918e6d41e3bae247a5ef3be37affc
rvpanoz <rvpanoz@gmail.com>
2018-08-20 11:39:44 +0300

    create downloads folder if not exists

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const config = {
   client_id: "KFSHpN5xEaAvIZZCrsrDjuFHOcArM91q",
   hostApi: "api.soundcloud.com",
   resolveUri: "http://api.soundcloud.com/resolve",
+  initial: 'https://soundcloud.com/atish/atish-075-mixmag-lab-nyc'
 };
 
 export default config;
