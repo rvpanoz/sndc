90d5816af1b94d0cf571e3c4403d0cc86b01719b
rvpanoz <rvpanoz@gmail.com>
2018-08-20 11:42:06 +0300

    remove initial soundcloud url

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ const config = {
   _wait: 500,
   client_id: "KFSHpN5xEaAvIZZCrsrDjuFHOcArM91q",
   hostApi: "api.soundcloud.com",
-  resolveUri: "http://api.soundcloud.com/resolve",
-  initial: 'https://soundcloud.com/atish/atish-075-mixmag-lab-nyc'
+  resolveUri: "http://api.soundcloud.com/resolve"
 };
 
 export default config;
